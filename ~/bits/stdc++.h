acf1ee7f453b0b73d4a200a92fb5ec4c1369ec0b
Usama Ali <usamaali.awan888@gmail.com>
2025-08-04 13:08:37 +0500

    new problem

---
+++ ~/bits/stdc++.h
@@ -0,0 +1,18 @@
+// ~/bits/stdc++.h
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <bitset>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <cstring>
+using namespace std;
